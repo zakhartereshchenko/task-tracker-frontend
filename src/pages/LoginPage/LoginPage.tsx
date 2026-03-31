af5f5e05b5f518f566c3377e473161661e8df10d
Zakhar <zahar.tereshencko20012@gmail.com>
2026-03-31 23:13:01 +0200

    changed styles

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -9,20 +9,26 @@ import { useLoginStore } from "../../store/login.store";
 export const LoginPage = () => {
     const mode = useLoginStore(state => state.mode)
     const setMode = useLoginStore(state => state.setMode)
+
+    const pageStyles = "w-1/2 h-full flex bg-cover bg-center bg-fit"
   
+    const formStyles = "w-full max-w-md flex flex-col items-center p-9 bg-transparent backdrop-blur-2xl rounded-2xl border border-white/15 shadow-2xl";
+
+    const sidePanelStyles = "w-150 h-full bg-primary flex flex-col gap-4 justify-center items-center text-white text-2xl font-bold";
+
     return (
         <div className="flex flex-col items-center justify-center gap-5 h-screen">
             <div className="w-full justify-around">
                 <div className="relative w-full h-screen overflow-hidden">
                     <AnimatedTransition mode={mode}>
-                        <div className="w-1/2 h-full flex bg-cover bg-center bg-fit" style={{ backgroundImage: `url(${bg})` }}>
+                        <div className={pageStyles} style={{ backgroundImage: `url(${bg})` }}>
                             <div className="w-full flex flex-col gap-15 justify-center items-center">
                                 <h2 className="text-white text-7xl">Welcome back</h2>
-                                <div className="w-full max-w-md flex flex-col items-center p-9 bg-transparent backdrop-blur-2xl rounded-2xl border border-gray-50">
+                                <div className={formStyles}>
                                     <SignInForm />
                                 </div>
                             </div>
-                            <div className= "w-200 h-full bg-primary flex flex-col gap-4 justify-center items-center text-white text-2xl font-bold">
+                            <div className={sidePanelStyles}>
                                 <h4>Don't have an account?</h4>
                                 <Button
                                     variant="outline"
@@ -34,8 +40,8 @@ export const LoginPage = () => {
                             </div>
                         </div>
 
-                        <div className="w-1/2 h-full flex bg-cover bg-center bg-fit" style={{ backgroundImage: `url(${bg})` }}>
-                            <div className= "w-200 h-full bg-primary flex flex-col gap-4 justify-center items-center text-white text-2xl font-bold">
+                        <div className={pageStyles} style={{ backgroundImage: `url(${bg})` }}>
+                            <div className={sidePanelStyles}>
                                 <h4>Already have an account?</h4>
                                 <Button
                                     variant="outline"
@@ -48,7 +54,7 @@ export const LoginPage = () => {
                             <div className="w-full h-full flex">
                                 <div className="w-full flex flex-col gap-15 justify-center items-center">
                                     <h2 className="text-white text-7xl">Registration</h2>
-                                    <div className="w-full max-w-md flex flex-col items-center p-9 bg-transparent backdrop-blur-2xl rounded-2xl border border-gray-50">
+                                    <div className={formStyles}>
                                         <SignUpForm />
                                     </div>
                                 </div>
